ee2f441c003ac6e481786a9d0e56cf2ac32e65b5
thorne7 <darienhallam@gmail.com>
2023-03-19 21:01:33 +1030

    tests all passing

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -17,11 +17,11 @@ const Employee = require("../lib/Employee");
 describe("Employee", () => {
     describe("Create an Employee object", () => {
         it("should create an Employee object with name, id, email properties", () => {
-            const employee = new Employee("John Doe", "001", "johndoe@test.com");
+            const employee = new Employee("John Doe", "001", "johndoe@test.com", "Manager");
             expect(employee.name).toEqual("John Doe");
             expect(employee.id).toEqual("001");
             expect(employee.email).toEqual("johndoe@test.com");
-            expect(employee.role).toEqual("Manage");
+            expect(employee.role).toEqual("Manager");
         });
     });
 });
--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -11,9 +11,9 @@ describe("Engineer", () => {
     it("should create an Engineer object with name, id, email and github properties", () => {
       const engineer = new Engineer("Paul Smith", "002", "paulsmith@test.com", "psmit");
       expect(engineer.name).toEqual("Paul Smith");
-      expect(intern.id).toEqual("002");
-      expect(intern.email).toEqual("paulsmith@test.com");
-      expect(intern.school).toEqual("psmit");
+      expect(engineer.id).toEqual("002");
+      expect(engineer.email).toEqual("paulsmith@test.com");
+      expect(engineer.github).toEqual("psmit");
     });
   });
 });
